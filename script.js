0f21112044cebebb85923498c04f2b1d0d0a05f8
Ari <arisenger0@gmail.com>
2024-01-15 14:49:30 +0100

    add: changing done state

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ const btnRemove = document.getElementById("btn-remove");
 const btnAdd = document.getElementById("btn-add");
 const todoInput = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
-// list state
-const state = {
-  todos: [
-    { description: "Learn driving a car", done: false, id: 1 },
-    { description: "Learn CSS", done: true, id: 2 },
-  ],
-};
+// state
+let state;
+if (localStorage.getItem("state")) {
+  state = JSON.parse(localStorage.getItem("state"));
+} else {
+  state = {
+    todos: [{ description: "Learn CSS", done: false, id: 1 }],
+  };
+}
 createTodo();
 // todos aus dem state im HTML erstellen
 function createTodo() {
+  todoList.innerHTML = "";
   for (const item of state.todos) {
     // list item
     const listItem = document.createElement("li");
@@ -23,6 +26,11 @@ function createTodo() {
     checkbox.type = "checkbox";
     checkbox.id = item.id;
     checkbox.checked = item.done;
+    // eventlistener for changind done state
+    checkbox.addEventListener("change", (event) => {
+      const doneState = event.target.checked;
+      item.done = doneState;
+    });
     // label
     const listLabel = document.createElement("label");
     listLabel.textContent = " " + item.description;
@@ -34,12 +42,28 @@ function createTodo() {
   }
 }
 
+// add todos
+function addTodos(e) {
+  e.preventDefault();
+  const todoValue = todoInput.value;
+
+  const newTodo = {};
+  newTodo.description = todoValue;
+  newTodo.done = false;
+  newTodo.id = createId(todoValue);
+  state.todos.push(newTodo);
+  // save in local storage
+  localStorage.setItem("state", JSON.stringify(state));
+
+  createTodo();
+}
+btnAdd.addEventListener("click", addTodos);
+
+// create Id function
+function createId(input) {
+  return input.replaceAll(" ", "").toLowerCase();
+}
 // get the input value
-btnAdd.addEventListener("click", function (event) {
-  event.preventDefault();
-  const todoValue = document.getElementById("todo-input").value;
-  console.log(todoValue.trim());
-});
 
 // remove button (just when todo is done!)
 btnRemove.addEventListener("click", function (event) {
