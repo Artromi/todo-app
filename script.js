9abffe01913c60ac42aba4025428cd87faa47069
Ari <arisenger0@gmail.com>
2024-01-15 09:43:15 +0100

    add:create function

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-const radioAll = document.getElementById("all");
-const radioOpen = document.getElementById("open");
-const radioDone = document.getElementById("done");
-const btmRemove = document.getElementById("btn-remove");
+const optionAll = document.getElementById("all");
+const optionOpen = document.getElementById("open");
+const optionDone = document.getElementById("done");
+const btnRemove = document.getElementById("btn-remove");
 const btnAdd = document.getElementById("btn-add");
 const todoInput = document.getElementById("todo-input");
-
 const todoList = document.getElementById("todo-list");
 // list state
-let todos = {};
-// create and append list element with input(checkbox) & label
-const listItem = document.createElement("li");
-todoList.appendChild(listItem);
-
-const checkbox = document.createElement("input");
-checkbox.setAttribute("type", "checkbox"); // unique name and for label ?
-listItem.appendChild(checkbox);
-
-const todoLabel = document.createElement("label");
-todoLabel.setAttribute("for", "checkbox"); // unique name and for label ?
-listItem.appendChild(todoLabel);
-
-todoLabel.textContent = "go for a walk";
-listItem.setAttribute("id", "todo"); // should uniquely identify the todo
+const state = {
+  todos: [
+    { description: "Learn driving a car", done: false, id: 1 },
+    { description: "Learn CSS", done: false, id: 2 },
+  ],
+};
+createTodo();
+// todos aus dem state im HTML erstellen
+function createTodo() {
+  for (const item of state.todos) {
+    // list item
+    const listItem = document.createElement("li");
+    // input
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = item.id;
+    checkbox.checked = item.done;
+    // label
+    const listLabel = document.createElement("label");
+    listLabel.textContent = item.description;
+    listLabel.setAttribute("for", item.id);
+    // append
+    listItem.appendChild(checkbox);
+    listItem.appendChild(listLabel);
+    todoList.appendChild(listItem);
+  }
+}
 
 // get the input value
 btnAdd.addEventListener("click", function (event) {
   event.preventDefault();
   const todoValue = document.getElementById("todo-input").value;
-  console.log(todoValue);
+  console.log(todoValue.trim());
+});
+
+// remove button (just when todo is done!)
+btnRemove.addEventListener("click", function (event) {
+  event.preventDefault();
+  todoList.innerHTML = "";
 });
