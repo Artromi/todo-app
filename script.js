c516c1df6623a087b270248df786031c81b40c06
Ari <arisenger0@gmail.com>
2024-01-18 10:36:57 +0100

    add:filter functions

--- script.js
+++ script.js
@@ -5,21 +5,23 @@ const optionsAll = document.getElementById("all");
 const optionsDone = document.getElementById("done");
 const optionsOpen = document.getElementById("open");
 const todoList = document.getElementById("todo-list");
+
 //
 // state
-let state = {
-  todos: [{ description: "learn HTML", id: "one", done: false }],
-};
-// localStorage.setItem("state", JSON.stringify(state));
+const state = localStorage.getItem("state")
+  ? JSON.parse(localStorage.getItem("state"))
+  : {
+      todos: [{ description: "learn HTML", id: "default", done: false }],
+    };
 //
 // call render function
 renderElements();
 //
 // FUNCTIONS //
 // renderElements function
-function renderElements() {
+function renderElements(currentStatus = state.todos) {
   todoList.innerHTML = "";
-  for (const todo of state.todos) {
+  for (const todo of currentStatus) {
     // create elements
     const listItem = document.createElement("li");
     const itemLabel = document.createElement("label");
@@ -28,6 +30,12 @@ function renderElements() {
     checkbox.type = "checkbox";
     checkbox.id = todo.id;
     checkbox.checked = todo.done;
+    // EventListener for changing done state
+    checkbox.addEventListener("change", function (e) {
+      const doneState = e.target.checked;
+      todo.done = doneState;
+      localStorage.setItem("state", JSON.stringify(state));
+    });
     itemLabel.textContent = " " + todo.description;
     itemLabel.setAttribute("for", todo.id);
     // append elements
@@ -57,7 +65,27 @@ function createId() {
   return date;
 }
 //
+// filter options
+function filterDone() {
+  const currentState = JSON.parse(localStorage.getItem("state"));
+  const doneTodos = currentState.todos.filter((todo) => todo.done === true);
+  renderElements(doneTodos);
+}
+function filterOpen() {
+  const currentState = JSON.parse(localStorage.getItem("state"));
+  const openTodos = currentState.todos.filter((todo) => todo.done === false);
+  renderElements(openTodos);
+}
+function filterAll() {
+  renderElements();
+}
+//
+// remove function
+function removeTodos() {}
+//
 // EVENT LISTENER //
 btnAdd.addEventListener("click", addTodo);
-
-////
+btnRemove.addEventListener("click", removeTodos);
+optionsDone.addEventListener("change", filterDone);
+optionsOpen.addEventListener("change", filterOpen);
+optionsAll.addEventListener("change", filterAll);
